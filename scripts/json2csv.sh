c341fb0130dda1dc4171c30754b2a275ebed21fc
Devan Fischer <contact@devanfischer.com>
2021-11-29 22:59:43 -0800

    Testing csv conversion

--- scripts/json2csv.sh
+++ scripts/json2csv.sh
@@ -9,8 +9,8 @@ for locale in ../client/public/locales/* ; do
 	do
 		filename=$(basename -- $file)
         # echo $filename
-		cp -r $file ../glossary/$localename/${filename%.*}.txt
-		# npx i18next-json-csv-converter ../public/locales/$localename/$filename ../glossary/$localename/${filename%.*}.csv
+		# cp -r $file ../glossary/$localename/${filename%.*}.txt
+		npx i18next-json-csv-converter $file ../glossary/$localename/${filename%.*}.csv
 		# npx i18next-json-csv-converter ../public/locales/$localename/$filename ../glossary/$localename/${filename%.*}.csv
 		# echo "" >> $rootdir/glossary/$localename/${filename%.*}.csv
 		# echo "glossary/${filename%.*}.csv"
