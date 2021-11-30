fb789bfebc46435b32f7bf7e431d28dd3646d13c
Devan Fischer <contact@devanfischer.com>
2021-11-30 08:07:48 -0800

    wip: tracking only the locales folder

--- scripts/json2csv.sh
+++ scripts/json2csv.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-for locale in ../client/public/locales/* ; do
+localeDir="../client/public/locales"
+
+for locale in $localeDir/*; do
 	localename=$(basename -- $locale)
 	mkdir -p ../glossary/$localename
-
-	for file in ../client/public/locales/$localename/*.json ; do
+	for file in $localeDir/$localename/*.json; do
 		filename=$(basename -- $file)
 		npx i18next-json-csv-converter $file ../glossary/$localename/${filename%.*}.csv
-		# echo "" >> $rootdir/glossary/$localename/${filename%.*}.csv
 	done
 done
