b1b648795dbee66a9cbc0291282bdddcaed1d1b8
Devan Fischer <contact@devanfischer.com>
2021-11-29 23:20:59 -0800

    fixing script

--- scripts/json2csv.sh
+++ scripts/json2csv.sh
@@ -3,10 +3,10 @@
 for locale in ../client/public/locales/* ; do
 	localename=$(basename -- $locale)
 	mkdir -p ../glossary/$localename
-	for file in ../client/public/locales/$localename/*.json
-	do
+
+	for file in ../client/public/locales/$localename/*.json ; do
 		filename=$(basename -- $file)
 		npx i18next-json-csv-converter $file ../glossary/$localename/${filename%.*}.csv
-		echo "" >> $rootdir/glossary/$localename/${filename%.*}.csv
+		# echo "" >> $rootdir/glossary/$localename/${filename%.*}.csv
 	done
 done
